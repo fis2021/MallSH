af459bff344235d6dac3c6ae86a0eb7081535fc4
dragosremetea <dragos.remetea@gmail.com>
2021-05-11 01:50:23 +0300

    [MSH-46] revert saving as favorite created

--- src/main/java/userr/controllers/ShowDetailController.java
+++ src/main/java/userr/controllers/ShowDetailController.java
@@ -5,22 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.dizitart.no2.NitriteId;
 import org.dizitart.no2.objects.ObjectRepository;
-import userr.exceptions.DuplicatedAdException;
-import userr.exceptions.FieldNotCompletedException;
-import userr.exceptions.TitleDoesNotMatchException;
-import userr.exceptions.WrongUsernameException;
+import userr.exceptions.*;
 import userr.model.Ad;
 import userr.model.User;
 import userr.services.AdService;
 import userr.services.FavoriteAdService;
-import userr.services.MyAdsService;
 import userr.services.UserService;
 
 import java.io.File;
@@ -52,16 +47,16 @@ public class ShowDetailController {
     @FXML
     Text about;
     @FXML
-    Text AddAdMessage;
+    Text NotFavMessage;
+
     @FXML
-    private static String loggedUsername;
     public static Ad ad1 = new Ad();
     @FXML
     public void initialize() throws IOException {
         about.setText(SearchDetailController.ad1.getDescription());
         for(Ad ad : favAdRepo.find()) {
             if (Objects.equals(LoginController.getLoggedUsername(), ad.getOwnUsername()) && Objects.equals(ad.isFavorite(), true)
-                    && Objects.equals(ad.getVusername(),SearchDetailController.ad1.getVusername())) {
+                    && Objects.equals(ad.getVusername(),SearchDetailController.ad1.getVusername()) && Objects.equals(ad.getTitle(),SearchDetailController.ad1.getTitle())) {
                 saveAdPicture();
             }
         }
@@ -100,17 +95,31 @@ public class ShowDetailController {
         window.setScene(Searchscene);
         window.show();
     }
-    public void handleSaveAdAction(javafx.event.ActionEvent search) throws IOException {
-        loggedUser = LoginController.getLoggedUsername();
-        //User loggedInUser=userRepository.find(eq("username",username)).firstOrDefault();
-        for(Ad ad: adRepository.find()) {
-            if (Objects.equals(Title.getText(), ad.getTitle()) && Objects.equals(User.getText(), ad.getVusername())) {
-                ad1 = ad;
+    public void handleSaveAdAction() throws IOException {
+            loggedUser = LoginController.getLoggedUsername();
+            if(!Objects.equals(Title.getText(),ad1.getTitle()) || !Objects.equals(loggedUser,ad1.getOwnUsername())
+                    || !Objects.equals(User.getText(),ad1.getVusername()))
+            {
+                ad1.setFavorite(false);
             }
-        }
-        String id = NitriteId.newId().toString();
-        FavoriteAdService.addFavorite(id,loggedUser, ad1.getTitle(),ad1.getVusername(),true);
-        saveAdPicture();
+            for (Ad ad : adRepository.find()) {
+                if (Objects.equals(Title.getText(), ad.getTitle()) && Objects.equals(User.getText(), ad.getVusername()) && Objects.equals(ad1.isFavorite(), false)) {
+                    ad1 = ad;
+                    ad1.setOwnUsername(loggedUser);
+                    ad1.setFavorite(false);
+                }
+            }
+            if (!Objects.equals(ad1.isFavorite(), false) && Objects.equals(ad1.getOwnUsername(), LoginController.getLoggedUsername())
+                    && Objects.equals(ad1.getVusername(), User.getText()) && Objects.equals(ad1.getTitle(), Title.getText())) {
+                FavoriteAdService.deleteFavorite(loggedUser, ad1.getTitle(), ad1.getVusername());
+                ad1.setFavorite(false);
+                undoSaveAdPicture();
+            } else {
+                String id = NitriteId.newId().toString();
+                FavoriteAdService.addFavorite(id, loggedUser, ad1.getTitle(), ad1.getVusername(), true);
+                ad1.setFavorite(true);
+                saveAdPicture();
+            }
     }
 
     private void saveAdPicture() throws MalformedURLException {
@@ -124,6 +133,18 @@ public class ShowDetailController {
         savePath.setFitHeight(53);
         savePath.setImage(saveImg);
     }
+    private void undoSaveAdPicture() throws MalformedURLException {
+        String pathUser = "src/main/resources/green_h.png";
+        File file = new File(pathUser);
+        String localUrl = file.toURI().toURL().toExternalForm();
+        Image saveImg = new Image(localUrl, false);
+        savePath.setLayoutY(396);
+        savePath.setLayoutX(594);
+        savePath.setFitWidth(74);
+        savePath.setFitHeight(53);
+        savePath.setImage(saveImg);
+    }
+
     public void minimizeWindow(javafx.event.ActionEvent min) {
         Stage window = (Stage) ((Node)min.getSource()).getScene().getWindow();
         window.setIconified(true);
